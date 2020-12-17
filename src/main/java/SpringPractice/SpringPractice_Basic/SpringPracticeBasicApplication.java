7c3c70a22c5d34ebcf44da4bb6a18504e5931447
eunhyeok8903 <eunhyeok8903@gmail.com>
2020-12-17 19:32:39 +0900

    Initial commit

---
+++ src/main/java/SpringPractice/SpringPractice_Basic/SpringPracticeBasicApplication.java
@@ -0,0 +1,13 @@
+package SpringPractice.SpringPractice_Basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SpringPractice/SpringPractice_Basic/SpringPracticeBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringPractice.SpringPractice_Basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
